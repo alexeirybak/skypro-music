de0c563f3bb90259d58d4baa3c24e47398fd9f3b
alexeirybak <rybak2108@yandex.ru>
2023-10-02 12:44:25 +0300

    a bit corrected

--- src/components/NavTrackSidebar/index.jsx
+++ src/components/NavTrackSidebar/index.jsx
@@ -5,26 +5,14 @@ import * as S from './styles.js';
 
 export const NavTrackSidebar = ({
   isLoading,
-  music,
-  error,
-  setIsPlaying,
-  setIsBar,
-  currentTrack,
-  setCurrentTrack,
-  pause,
+  ...props
 }) => {
   return (
     <S.NavTrackSidebar>
       <Nav />
       <SidebarCenterBlock
-        pause={pause}
-        isLoading={isLoading}
-        music={music}
-        setIsPlaying={setIsPlaying}
-        setIsBar={setIsBar}
-        currentTrack={currentTrack}
-        setCurrentTrack={setCurrentTrack}
-        error={error}
+      isLoading={isLoading}
+        {...props}
       />
       <MainSidebar isLoading={isLoading} />
     </S.NavTrackSidebar>
