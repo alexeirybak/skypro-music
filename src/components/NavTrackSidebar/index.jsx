747282038f0207dc5d7dc620c748c1344dc76bbd
alexeirybak <128369349+alexeirybak@users.noreply.github.com>
2023-10-04 20:03:17 +0200

    Add files via upload

---
+++ src/components/NavTrackSidebar/index.jsx
@@ -0,0 +1,20 @@
+import { Nav } from '../Nav';
+import { SidebarCenterBlock } from '../SidebarCenterBlock';
+import { MainSidebar } from '../MainSidebar'; 
+import * as S from './styles.js';
+ 
+export const NavTrackSidebar = ({
+  isLoading,
+  ...props
+}) => {
+  return (
+    <S.NavTrackSidebar>
+      <Nav />
+      <SidebarCenterBlock
+      isLoading={isLoading}
+        {...props}
+      />
+      <MainSidebar isLoading={isLoading} />
+    </S.NavTrackSidebar>
+  );
+};
