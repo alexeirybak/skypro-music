b4973263606cce1ff5def1b7450a0f09d9ccb7d3
alexeirybak <rybak2108@yandex.ru>
2023-10-11 23:09:20 +0300

    Redone

--- src/components/MainBar/MainBar.jsx
+++ src/components/MainBar/MainBar.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import PlayerProgress from './playerProgress';
 import PlayerBlock from './playerBlock';
 import * as S from './styles';
@@ -6,7 +6,32 @@ import * as S from './styles';
 function MainBar({ playTrack }) {
   const audioRef = useRef(null);
   const [repeat, setRepeat] = useState(false);
-  console.log(playTrack);
+  const [value, setValue] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    if (audio) {
+      const onLoadedMetadata = () => setDuration(audio.duration);
+      const onTimeUpdate = () => setValue(audio.currentTime);
+
+      audio.addEventListener('loadedmetadata', onLoadedMetadata);
+      audio.addEventListener('timeupdate', onTimeUpdate);
+
+      return () => {
+        audio.removeEventListener('loadedmetadata', onLoadedMetadata);
+        audio.removeEventListener('timeupdate', onTimeUpdate);
+      };
+    }
+  }, []);
+
+  const handleInputChange = (newValue) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = newValue;
+      setValue(newValue);
+    }
+  };
 
   return (
     <S.Bar>
@@ -17,7 +42,11 @@ function MainBar({ playTrack }) {
           autoPlay
           loop={repeat}
         />
-        <PlayerProgress audioRef={audioRef} />
+        <PlayerProgress
+          value={value}
+          duration={duration}
+          onInputChange={handleInputChange}
+        />
         <PlayerBlock
           repeat={repeat}
           setRepeat={setRepeat}
--- src/components/MainBar/playerProgress/index.jsx
+++ src/components/MainBar/playerProgress/index.jsx
@@ -1,71 +1,22 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import * as S from './styles';
-import {
-  addActiveTrack,
-  addPlayTrack,
-} from '../../../store/actions/creators/creators';
-import allTracksSelector, {
-  activeTrackSelector,
-  shuffleTracksSelector,
-} from '../../../store/selectors/selectors';
 import formatTime from '../../Helper/Helper';
 
-function PlayerProgress({ audioRef }) {
-  const [value, setValue] = useState('1');
-  const [duration, setDuration] = useState('0');
-  const allTracks = useSelector(allTracksSelector);
-  const activeTrack = useSelector(activeTrackSelector);
-  const shuffleTrack = useSelector(shuffleTracksSelector);
-  const dispatch = useDispatch();
-
-  const toggleProgress = (e) => {
-    audioRef.current.currentTime = e.target.value;
+function PlayerProgress({ value, duration, onInputChange }) {
+  const handleChange = (e) => {
+    onInputChange(e.target.value);
   };
-
-  useEffect(() => {
-    if (audioRef?.current.duration) {
-      setDuration(audioRef.current.duration);
-    }
-  });
-
-  useEffect(() => {
-    if (audioRef?.current.currentTime) {
-      audioRef.current.addEventListener('timeupdate', () => {
-        setValue(audioRef.current.currentTime);
-        return () => {
-          audioRef.current.removeEventListener('timeupdate', () => {
-            setValue(audioRef.current.currentTime);
-          });
-        };
-      });
-    }
-  });
-
-  useEffect(() => {
-    if (value === duration) {
-      if (activeTrack.index === allTracks.length - 1) return;
-      dispatch(
-        addActiveTrack({ ...activeTrack, index: activeTrack.index + 1 }),
-      );
-      if (activeTrack.shuffle) {
-        dispatch(addPlayTrack(shuffleTrack[activeTrack.index + 1]));
-      } else {
-        dispatch(addPlayTrack(allTracks[activeTrack.index + 1]));
-      }
-    }
-  }, [value]);
 
   return (
     <>
       <S.PlayerProgressInput
-        onInput={(e) => toggleProgress(e)}
         type='range'
-        id='volume'
-        min='0'
+        id='progress'
+        min={0}
         max={duration}
         value={value}
-        step='0.01'
+        step={0.01}
+        onChange={handleChange}
       />
       <S.BarTime>
         {formatTime(Math.floor(value))} / {formatTime(Math.floor(duration))}
